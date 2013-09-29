86f3ff244186e7960422b178d0f26b553ef07be6
jcerovec <jcerovec@gmail.com>
2013-09-30 00:07:53 +0200

    [SDK] PPDocumentsTableDataSource now uses PPDateSortedDocumentsSectionCreator as default

--- PhotoPayCloudSDK/PhotoPayCloudSDK/Classes/View/PPDocumentsTableDataSource.m
+++ PhotoPayCloudSDK/PhotoPayCloudSDK/Classes/View/PPDocumentsTableDataSource.m
@@ -11,13 +11,36 @@
 #import "PPLocalDocument.h"
 #import "PPRemoteDocument.h"
 #import "PPLocalDocumentUploadQueue.h"
-#import "PPTableSectionCreator.h"
+#import "PPDateSortedDocumentsSectionCreator.h"
+#import <UIKit/UIKit.h>
 
 @interface PPDocumentsTableDataSource ()
+
+/**
+ A list of all items currently in data source
+ */
+@property (nonatomic, strong, readonly) NSMutableArray* items;
+
+/**
+ A list of all items currently in data source, divided into section
+ */
+@property (nonatomic, strong, readonly) NSArray* sections;
 
 @end
 
 @implementation PPDocumentsTableDataSource
+
+@synthesize items;
+@synthesize sectionCreator;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        items = [[NSMutableArray alloc] init];
+        sectionCreator = [[PPDateSortedDocumentsSectionCreator alloc] init];
+    }
+    return self;
+}
 
 - (void)insertItems:(NSArray*)itemsToAdd {
     NSIndexPath *indexPath = nil;
@@ -30,6 +53,25 @@
         if (index == NSNotFound) {
             [[self items] addObject:item];
             indexPath = [[self sectionCreator] insertItem:item];
+            
+            // new object is inserted, compensate in index paths that were already inserted
+            for (int i = 0; i < [insertedIndexPaths count]; i++) {
+                NSIndexPath *ip = [insertedIndexPaths objectAtIndex:i];
+                if ([ip section] == [indexPath section] && [ip row] >= [indexPath row]) {
+                    NSIndexPath *newip = [NSIndexPath indexPathForRow:ip.row+1 inSection:ip.section];
+                    [insertedIndexPaths replaceObjectAtIndex:i withObject:newip];
+                }
+            }
+            
+            // new object is inserted, compensate in index paths that were already inserted
+            for (int i = 0; i < [reloadedIndexPaths count]; i++) {
+                NSIndexPath *ip = [reloadedIndexPaths objectAtIndex:i];
+                if ([ip section] == [indexPath section] && [ip row] >= [indexPath row]) {
+                    NSIndexPath *newip = [NSIndexPath indexPathForRow:ip.row+1 inSection:ip.section];
+                    [reloadedIndexPaths replaceObjectAtIndex:i withObject:newip];
+                }
+            }
+            
             [insertedIndexPaths addObject:indexPath];
         } else {
             id object = [[self items] objectAtIndex:index];
